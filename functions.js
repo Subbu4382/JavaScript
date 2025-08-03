384b133620d9ba85619618c99bc5f8fd8d7955b5
Subbu4382 <subrahmanyamdunne@gmail.com>
2025-08-03 11:08:45 +0530

    Functions

---
+++ functions.js
@@ -0,0 +1,34 @@
+// function hello(){
+//     console.log("Good Morning")
+// }
+// hello()
+
+// function named(name){
+//     console.log( "Welcome",name)
+// }
+// named("subbu")
+
+
+// function sum(a,b){
+//     return a+b
+// }
+// x=sum(4,7)
+// console.log(x)
+
+// function even(a){
+//     if (a%2==0){
+//       console.log("even")
+//     }
+//     else{
+//         console.log("odd")
+//     }
+// }
+// even(4)
+
+
+// function loop(n){
+//     for(let i=0;i<=n;i++){
+//         console.log(i)
+//     }
+// }
+// loop(5)
