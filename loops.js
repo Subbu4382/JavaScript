43417609327b1c84c4f3cc8c622aab653c0c9eac
Subbu4382 <subrahmanyamdunne@gmail.com>
2025-08-03 17:38:45 +0530

    using math and random in javascript

--- loops.js
+++ loops.js
@@ -1,37 +1,40 @@
-// loops
-for (let i=0;i<=10;i++){
-    console.log(i);
-}
+// // loops
+// for (let i=0;i<=10;i++){
+//     console.log(i);
+// }
 
-num=prompt("Enter a number :")
-var i=1;
-while(i<=num){
-    console.log(i);
-    i++;
-}
+// num=prompt("Enter a number :")
+// var i=1;
+// while(i<=num){
+//     console.log(i);
+//     i++;
+// }
 
-num=prompt('enter a number')
-var j=2;
-do{
-    console.log(j);
-    j=j+2
-}
-while (j<=num);
+// num=prompt('enter a number')
+// var j=2;
+// do{
+//     console.log(j);
+//     j=j+2
+// }
+// while (j<=num);
 
-// Find the sum of numbers from 1 to 100
+// // Find the sum of numbers from 1 to 100
 
-num=prompt('enter a number : ')
-var sum=0;
-for(let i=0;i<=num;i++){
-    sum=sum+i
-}
-console.log(sum)
+// num=prompt('enter a number : ')
+// var sum=0;
+// for(let i=0;i<=num;i++){
+//     sum=sum+i
+// }
+// console.log(sum)
 
-// Reverse a string using a loop
+// // Reverse a string using a loop
 
-let s="javaScript"
-let res=""
-for(let i=9;i>=0;i--){
-    res=res+s[i]
-}
-console.log(res);
+// let s="javaScript"
+// let res=""
+// for(let i=9;i>=0;i--){
+//     res=res+s[i]
+// }
+// console.log(res);
+
+var x=Math.ceil(Math.random()*100)
+console.log(x)
