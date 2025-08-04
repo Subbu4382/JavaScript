b16ea1fb3289ba8fe508a248c79454b56d3d49d6
Subbu4382 <subrahmanyamdunne@gmail.com>
2025-08-04 23:22:56 +0530

    Callback Functions

--- functions.js
+++ functions.js
@@ -1,78 +1,78 @@
-// function hello(){
-//     console.log("Good Morning")
-// }
-// hello()
+function hello(){
+    console.log("Good Morning")
+}
+hello()
 
-// function named(name){
-//     console.log( "Welcome",name)
-// }
-// named("subbu")
-
-
-// function sum(a,b){
-//     return a+b
-// }
-// x=sum(4,7)
-// console.log(x)
-
-// function even(a){
-//     if (a%2==0){
-//       console.log("even")
-//     }
-//     else{
-//         console.log("odd")
-//     }
-// }
-// even(4)
+function named(name){
+    console.log( "Welcome",name)
+}
+named("subbu")
 
 
-// function loop(n){
-//     for(let i=0;i<=n;i++){
-//         console.log(i)
-//     }
-// }
-// loop(5)
+function sum(a,b){
+    return a+b
+}
+x=sum(4,7)
+console.log(x)
+
+function even(a){
+    if (a%2==0){
+      console.log("even")
+    }
+    else{
+        console.log("odd")
+    }
+}
+even(4)
+
+
+function loop(n){
+    for(let i=0;i<=n;i++){
+        console.log(i)
+    }
+}
+loop(5)
 
 
 
 // Arrow Functions
 
-// const greet =()=> "i love you"
-// console.log(greet())
+const greet =()=> "i love you"
+console.log(greet())
 
-// const mul=(a,b)=> {return a*b}
-// console.log(mul(2,6))
+const mul=(a,b)=> {return a*b}
+console.log(mul(2,6))
 
 
-// const neg = (num) =>{
-//     if (num<0){
-//         return "negative"
-//     }
-// }
-// console.log(neg(-5))
+const neg = (num) =>{
+    if (num<0){
+        return "negative"
+    }
+}
+console.log(neg(-5))
 
-// const odd_even =(x) =>{
-//     if (x%2==0){
-//         return "even"
-//     }
-//     else{
-//         return "Odd"
-//     }
-// }
-// console.log(odd_even(8))
+const odd_even =(x) =>{
+    if (x%2==0){
+        return "even"
+    }
+    else{
+        return "Odd"
+    }
+}
+console.log(odd_even(8))
 
 //  converting named functions to arrow functions
 
-// function greet(){
-//     console.log("Hello, friends")
-// }
+function greet(){
+    console.log("Hello, friends")
+}
 
 const greet=()=> "Hello , friends"
 console.log(greet())
 
-// function square(num){
-//   console.log(num*num)
-// }
+function square(num){
+  console.log(num*num)
+}
 const square =(num)=>num*num
 console.log(square(22))
 
@@ -89,3 +89,15 @@ function cube(x){
     console.log(x**3)
 }
 cube(5)
+
+// call back functions
+// calling a function as a parameter in another function
+
+function greet(saybye){
+    console.log("hi");
+    saybye();
+}
+function saybye(){
+    console.log("bye")
+}
+greet(saybye)
