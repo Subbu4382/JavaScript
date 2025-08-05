27adbcf5aad9eb8ad10029b6a339b359f1e8f2cb
Subbu4382 <subrahmanyamdunne@gmail.com>
2025-08-05 17:05:21 +0530

    arrays

---
+++ arrays_objects.js
@@ -0,0 +1,12 @@
+const fruits=['banana','orange','apple','mango']
+console.log(fruits)
+console.log(fruits[2])
+console.log(fruits.length)
+console.log(fruits[fruits.length-1])
+console.log(typeof(fruits))
+fruits.push("cherry")
+console.log(fruits)
+fruits[0]='watermelon'
+console.log(fruits)
+fruits.pop()
+console.log(fruits)
