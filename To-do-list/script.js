ed3a8e841c1e713ee8ebdc7c49988056bf727162
Subbu4382 <subrahmanyamdunne@gmail.com>
2025-08-18 22:49:51 +0530

    to-do-list

---
+++ To-do-list/script.js
@@ -0,0 +1,39 @@
+let todoinput = document.getElementById("todo-input");
+let addbutton = document.getElementById("add-button");
+let todolist = document.getElementById("todo-list");
+
+addbutton.addEventListener("click", function () {
+  const todoText = todoinput.value;
+
+  if (todoText === "") {
+    alert("You must write something!");
+    return;
+  }
+
+  const li = document.createElement("li");
+
+  const span = document.createElement("span");
+  span.textContent = todoText;
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "Delete";
+  deleteBtn.className = "delete-btn";
+
+  li.appendChild(span);
+  li.appendChild(deleteBtn);
+
+  todolist.appendChild(li);
+
+  todoinput.value = "";
+});
+todolist.addEventListener("click", function (event) {
+  const clickedElement = event.target;
+
+  if (clickedElement.tagName === "SPAN") {
+    clickedElement.parentNode.classList.toggle("completed");
+  }
+
+  if (clickedElement.className === "delete-btn") {
+    clickedElement.parentNode.remove();
+  }
+});
