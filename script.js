d322889ad95764eed805ab55e070daf796e01954
Subbu4382 <subrahmanyamdunne@gmail.com>
2025-08-21 00:21:26 +0530

    code changed

--- script.js
+++ script.js
@@ -8,16 +8,34 @@
 //       body.style.backgroundColor = "white";
 //       body.style.color = "darkgreen";
 //     }
-localStorage.getItem("dark_mode") ? "yes" : "no"
+// let container = document.body
+// let theme=localStorage.getItem("dark_mode") ? "dark_mode" : "light_mode"
+// container.classList=theme
+// function changeTheme(){
+//   if(container.classlist=="dark_mode"){
+//   container.classlist="light_mode"
+//     localStorage.setItem("light_mode","light_mode")
+//     localStorage.removeItem("dark_mode")
+//   }
+//   else{
+//     container.classlist="dark_mode"
+//     localStorage.setItem("dark_mode","dark_mode")
+//      localStorage.removeItem("light_mode")
+//   }
+// }
 
-function changeTheme(){
-  let container = document.getElementById("container")
-  if(container.classlist=="dark_mode"){
-    container.classlist="light_mode"
-    localStorage.setItem("light_mode","light_mode")
-  }
-  else{
-    container.classlist="dark_mode"
-    localStorage.setItem("light_mode","light_mode")
-  }
-}
+let container = document.body;
+
+    // Load theme from localStorage OR default to light_mode
+    let theme = localStorage.getItem("theme") || "light_mode";
+    container.className = theme;
+
+    function changeTheme() {
+      if (container.className === "dark_mode") {
+        container.className = "light_mode";
+        localStorage.setItem("theme", "light_mode");
+      } else {
+        container.className = "dark_mode";
+        localStorage.setItem("theme", "dark_mode");
+      }
+    }
