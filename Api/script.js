1574fc0ab7bf385e85d0cd834628b52ea1425955
Subbu4382 <subrahmanyamdunne@gmail.com>
2025-08-28 17:20:01 +0530

    Fetching Apis

---
+++ Api/script.js
@@ -0,0 +1,14 @@
+let user_container=document.getElementById("users_data")
+fetch("https://jsonplaceholder.typicode.com/todos/")
+.then(function(response){
+    return response.json()
+})
+.then(function(jsondata){
+    // console.log(jsondata)
+    for (let i=0;i<jsondata.length;i++){
+        let card =document.createElement("div")
+        card.classList="card"
+        card.innerHTML=`<p>${jsondata[i].id}</p> <p>${jsondata[i].title}</p>`
+        user_container.appendChild(card)
+    }
+})
