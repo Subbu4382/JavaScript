93e5c7909e31add2947d031bc950b2f25bb2e608
Subbu4382 <subrahmanyamdunne@gmail.com>
2025-09-11 18:05:55 +0530

    image gallery

--- image_gallery/script.js
+++ image_gallery/script.js
@@ -5,12 +5,15 @@ let nextbtn = document.getElementById("nextbtn");
 scrollContainer.addEventListener("wheel", (evt) => {
   evt.preventDefault();
   scrollContainer.scrollLeft += evt.deltaY;
+  scrollContainer.style.scrollBehaviour = "auto";
 });
 
 nextbtn.addEventListener("click", () => {
+  scrollContainer.style.scrollBehaviour = "smooth";
   scrollContainer.scrollLeft += 900;
 });
 
 backbtn.addEventListener("click", () => {
+  scrollContainer.style.scrollBehaviour = "smooth";
   scrollContainer.scrollLeft -= 900;
 });
