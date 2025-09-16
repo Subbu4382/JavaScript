0ce247138c70b6cf13f650f38742a10116fbbb53
Subbu4382 <subrahmanyamdunne@gmail.com>
2025-09-16 16:35:46 +0530

    heart animation

--- heart_animation/script.js
+++ heart_animation/script.js
@@ -1,13 +1,21 @@
 const body = document.querySelector("body");
+let heading = document.querySelector("h2");
+setTimeout(() => {
+  heading.remove();
+}, 8000);
 body.addEventListener("mousemove", (event) => {
-  const xpos = event.offsetX;
-  const ypos = event.offsetY;
+  console.log("moving");
+  //   const xpos = event.offsetX;
+  //   const ypos = event.offsetY;
+  // const hearts = ["💖", "❤️", "💜", "💙", "💚", "🧡", "💛"];
   const span = document.createElement("span");
-  span.style.left = xpos + "px";
-  span.style.top = ypos + "px";
+  //   span.innerText = hearts[Math.floor(Math.random() * hearts.length)];
+  span.style.left = event.offsetX + "px";
+  span.style.top = event.offsetY + "px";
   const size = Math.random() * 100;
   span.style.width = size + "px";
   span.style.height = size + "px";
+  //   span.style.fontSize=size + "px"
   body.appendChild(span);
   setTimeout(() => {
     span.remove();
