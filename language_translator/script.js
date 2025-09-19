a3ad11df16e172faa43ceddf83811725dfce4349
Subbu4382 <subrahmanyamdunne@gmail.com>
2025-09-19 14:26:35 +0530

    language translator

--- language_translator/script.js
+++ language_translator/script.js
@@ -1,14 +1,16 @@
 const fromText = document.querySelector(".from-text"),
-toText = document.querySelector(".to-text"),
-exchageIcon = document.querySelector(".exchange"),
-selectTag = document.querySelectorAll("select"),
-icons = document.querySelectorAll(".row i");
-translateBtn = document.querySelector("button"),
+      toText = document.querySelector(".to-text"),
+      exchageIcon = document.querySelector(".exchange"),
+      selectTag = document.querySelectorAll("select"),
+      icons = document.querySelectorAll(".row i"),
+      translateBtn = document.querySelector("button");
+
 
 selectTag.forEach((tag, id) => {
     for (let country_code in countries) {
         let selected = id == 0 ? country_code == "en-GB" ? "selected" : "" : country_code == "hi-IN" ? "selected" : "";
-        let option = <option ${selected} value="${country_code}">${countries[country_code]}</option>;
+       let option = `<option ${selected} value="${country_code}">${countries[country_code]}</option>`;
+
         tag.insertAdjacentHTML("beforeend", option);
     }
 });
@@ -34,7 +36,8 @@ translateBtn.addEventListener("click", () => {
     translateTo = selectTag[1].value;
     if(!text) return;
     toText.setAttribute("placeholder", "Translating...");
-    let apiUrl = https://api.mymemory.translated.net/get?q=${text}&langpair=${translateFrom}|${translateTo};
+    let apiUrl = `https://api.mymemory.translated.net/get?q=${text}&langpair=${translateFrom}|${translateTo}`;
+
     fetch(apiUrl).then(res => res.json()).then(data => {
         toText.value = data.responseData.translatedText;
         data.matches.forEach(data => {
@@ -65,6 +68,6 @@ icons.forEach(icon => {
                 utterance.lang = selectTag[1].value;
             }
             speechSynthesis.speak(utterance);
-     }
-});
+        }
+    });
 });
